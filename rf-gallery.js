1150d0e3cc0ed673ec31560ffb2b1da7cd55b9af
Rex Flores <Rexgflores@gmail.com>
2020-12-23 11:49:12 -0800

    can search and pull iamges from API based on Pokedex number

---
+++ rf-gallery.js
@@ -0,0 +1,25 @@
+var $cardNum = $("#card-search").val().trim();
+console.log($cardNum)
+var $searchButton = $("#search-button");
+
+$searchButton.click(function (event) {
+  var $cardNum = $("#card-search").val().trim();
+  var queryURL = `https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=${$cardNum}`;
+
+  event.preventDefault()
+  console.log(queryURL)
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
+  
+    var cardImage = $("<img>")
+    cardImage.attr("src", data.cards[1].imageUrl);
+    $(".gallery").append(cardImage)
+   
+   forEach() // $(".gallery").prepend($("<img>", {id:'$cardName', src: "data.cards[1].imageUrl"}));
+});
+  
+})
