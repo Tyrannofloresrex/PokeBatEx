ac95be5fcf17f6ea0b6b88a02e0dbbed82abfa6d
Rex Flores <Rexgflores@gmail.com>
2020-12-24 14:22:12 -0800

    committing just in case, merging with dev from 12/23/2020

--- rf-gallery.js
+++ rf-gallery.js
@@ -1,7 +1,7 @@
 var $cardNum = $("#card-search").val().trim();
 console.log($cardNum)
 var $searchButton = $("#search-button");
-
+var $gallery = $(".gallery")
 $searchButton.click(function (event) {
   var $cardNum = $("#card-search").val().trim();
   var queryURL = `https://api.pokemontcg.io/v1/cards?nationalPokedexNumber=${$cardNum}`;
@@ -15,11 +15,24 @@ $searchButton.click(function (event) {
   }).then(function (data) {
     console.log(data);
   
-    var apiImage = $("<img>")
-    apiImage.attr("src", data.cards[1].imageUrl);
-    $(".gallery").append(apiImage)
-   
-   forEach() // $(".gallery").prepend($("<img>", {id:'$cardName', src: "data.cards[1].imageUrl"}));
-});
+    // var apiImage = $("<img>")
+    // apiImage.attr("src", data.cards[1].imageUrl);
+    // $(".gallery").append(apiImage)
+    var pokemonObject = {
+      imgURL: data.cards[1].imageUrl,
+      pokedexNum: data.cards[1].nationalPokedexNumber
+
+    }
+   render(data.cards[1].imageUrl, $gallery)
   
-})
+    
+});
+
+  
+});
+
+function render(imgUrlToAppend, parentElement){
+  var apiImage = $("<img>")
+    apiImage.attr("src", imgUrlToAppend);
+    parentElement.append(apiImage)
+}
